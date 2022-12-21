33629c9ce10ddcb55521cd830a6d31ce0c2dd2f0
n0tfilzhang <filizhang04@gmail.com>
2022-12-21 16:41:04 +0100

    n0tfilzhang 2.0

--- script.js
+++ script.js
@@ -5,22 +5,3 @@ document.addEventListener('DOMContentLoaded', (e) =>{
         splash.classList.add('display-none')
     }, 2000);
 })
-
-setInterval(createSnowFlake, 50);
-
-function createSnowFlake()
-{
-    const snowFlake = document.createElement('i');
-    snowFlake.classList.add('bi');
-    snowFlake.classList.add('bi-snow');
-    snowFlake.style.left = Math.random() * window.innerWidth + 'px';
-    snowFlake.style.animationDuration = Math.random() * 3 + 2 + 's';
-    snowFlake.style.opacity = Math.random();
-    snowFlake.style.fontSize = Math.random() * 10 + 10 + 'px';
-
-    document.body.appendChild(snowFlake);
-
-    setTimeout(() => {
-        snowFlake.remove();
-    }, 5000)
-}
